9ffc971a8ca03e49b45104d9b84df0df21fe7681
arvenbekirovik <514816@student.fontys.nl>
2023-12-19 14:30:05 +0100

    Klikfunctie compleet

--- test repo/Program.cs
+++ test repo/Program.cs
@@ -1,4 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("hello eartt");
+var i = 0;
 
+while (true) 
+{
+    i += 2;
+    Console.WriteLine("tink...");
+    if (i == 4)
+    {
+        Console.WriteLine("poop");
+    } else if (i == 6)
+	{
+        i = 0;
+        Console.Clear();
+        i = 0;
+    }
+    Thread.Sleep(1000);
+}
